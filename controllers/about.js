37de2e2a4fbcb807fa9d4152b017a2f630b77a8b
kevinegstorf <kevin.egstorf@gmail.com>
2014-12-04 23:44:25 +0100

    added controlers

---
+++ controllers/about.js
@@ -0,0 +1,15 @@
+Blogger.AboutController = Ember.Controller.extend({
+	isPictureShowing: false,
+actions: {
+	showRealName: function(){
+		alert('Dracula is fictional, but was inspired by the 15th-century Romanian general and Wallachian Prince Vlad II the impaler.');
+	},
+	showPicture: function() {
+		this.set('isPictureShowing', true);
+
+	},
+	hidePicture: function(){
+		this.set('isPictureShowing', false);
+	}
+}
+});
